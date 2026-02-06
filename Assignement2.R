2440d801ccc04f26290cde66f455bc7fd1157762
japhet125 <124853679+japhet125@users.noreply.github.com>
2026-02-05 23:39:11 -0500

    add code into movies rating and connect sql to r

--- Assignement2.R
+++ Assignement2.R
@@ -1,8 +1,31 @@
-install.packages("DBI")
+#install.packages("DBI")
 library(DBI)
-install.packages("RPostgres")
+#install.packages("RPostgres")
 library(RPostgres)
-install.packages("dplyr")
+#install.packages("dplyr")
 library(dplyr)
-install.packages("tidyr")
+#install.packages("tidyr")
 library(tidyr)
+Sys.setenv(PGPASSWORD = "newpassword")
+
+
+con <- dbConnect(
+  RPostgres::Postgres(),
+  dbname   = "Ass2A",
+  host     = "127.0.0.1",       # Local server
+  port     = 5432,              # Default PostgreSQL port
+  user     = "guibrilramde",    # Your pgAdmin username
+  password = Sys.getenv("PGPASSWORD")
+)
+
+# Check that tables exist
+DBI::dbListTables(con)
+
+
+users = dbReadTable(con, "users")
+movies = dbReadTable(con, "movies")
+ratings = dbReadTable(con, "ratings")
+
+users
+movies
+ratings
