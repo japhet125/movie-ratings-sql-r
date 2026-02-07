c648c3c96f1fe23d512822233d6587a03dc6d29e
japhet125 <124853679+japhet125@users.noreply.github.com>
2026-02-06 21:28:39 -0500

    finalizing the assignement 2A  creating table and analyzing the mean and average movie rating and user rating

--- Assignement2.R
+++ Assignement2.R
@@ -6,9 +6,14 @@ library(RPostgres)
 library(dplyr)
 #install.packages("tidyr")
 library(tidyr)
-Sys.setenv(PGPASSWORD = "newpassword")
 
 
+#install.packages("usethis")
+
+#usethis::edit_r_environ()
+
+
+#Dtabase connection
 con <- dbConnect(
   RPostgres::Postgres(),
   dbname   = "Ass2A",
@@ -18,7 +23,7 @@ con <- dbConnect(
   password = Sys.getenv("PGPASSWORD")
 )
 
-# Check that tables exist
+# Check that tables exist and load
 DBI::dbListTables(con)
 
 
@@ -26,6 +31,52 @@ users = dbReadTable(con, "users")
 movies = dbReadTable(con, "movies")
 ratings = dbReadTable(con, "ratings")
 
-users
-movies
-ratings
+head(users)
+head(movies)
+head(ratings)
+
+#building the rating dataset
+ratings_df = dbGetQuery(con, "
+SELECT
+    u.name AS user_name,
+    m.title AS movie_title,
+    r.rating
+FROM users u
+CROSS JOIN movies m
+LEFT JOIN ratings r
+    ON u.user_id = r.user_id
+    AND m.movie_id = r.movie_id
+   ")
+
+head(ratings_df)
+
+#Basic analysis getting the average rating from movies
+movie_summary = ratings_df %>%
+  group_by(movie_title) %>%
+  summarise(
+    avg_rating = mean(rating, na.rm = TRUE),
+    rating_count = sum(!is.na(rating))
+  ) %>%
+  arrange(desc(avg_rating))
+movie_summary
+
+#Basic analysis getting the average rating from each user
+user_summary = ratings_df %>%
+  group_by(user_name) %>%
+  summarise(
+    avg_rating = mean(rating, na.rm = TRUE),
+    movie_seen = sum(!is.na(rating))
+    
+  )
+user_summary 
+
+#the user item matrix
+rating_matrix = ratings_df %>%
+  pivot_wider(
+    names_from = movie_title,
+    values_from = rating
+  )
+
+rating_matrix
+
+dbDisconnect(con)
